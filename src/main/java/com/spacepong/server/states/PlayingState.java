f64802785429f1a465a6f6cc95a91b091e458a8b
ruben-bena <ruben.bellido.navarro@gmail.com>
2025-11-22 15:30:06 +0100

    Defined class PlayingState to send gameState to clients

--- src/main/java/com/spacepong/server/states/PlayingState.java
+++ src/main/java/com/spacepong/server/states/PlayingState.java
@@ -1,27 +1,34 @@
 package com.spacepong.server.states;
 
+import java.time.Instant;
+
 import org.json.JSONObject;
 
+import com.spacepong.server.enums.MessageType;
+import com.spacepong.server.enums.StateType;
+import com.spacepong.server.model.Game;
 import com.spacepong.server.services.GameSession;
+import com.spacepong.server.services.Logger;
 
 public class PlayingState implements GameState {
 
     @Override
     public void onEnter(GameSession session) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onEnter'");
+        Logger.log("Cambio a PlayingState");
     }
 
     @Override
     public void update(GameSession session) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        JSONObject payloadGameState = generateGameStateJson(session);
+        session.sendToBothPlayers(payloadGameState);
+        if (session.getGame().isGameFinished()) {
+            session.changeState(StateType.FINISHED);
+        }
     }
 
     @Override
     public void onExit(GameSession session) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onExit'");
+        Logger.log("Salgo de CountdownState");
     }
 
     @Override
@@ -30,4 +37,45 @@ public class PlayingState implements GameState {
         throw new UnsupportedOperationException("Unimplemented method 'handleMessage'");
     }
 
+    public JSONObject generateGameStateJson(GameSession session) {
+        /*
+        * JSON example:
+        *
+        *  {
+        *      "type": "gameState",
+        *      "ball": { "x": 0.50, "y": 0.82 },
+        *      "paddles": {
+        *          "p1": { "y": 0.30 },
+        *          "p2": { "y": 0.66 }
+        *      },
+        *      "score": { "p1": 3, "p2": 1 },
+        *      "timestamp": "2025-11-07T21:23:02.123Z"
+        *  }
+        * 
+        */
+        Game game = session.getGame();
+        JSONObject payloadGameState = new JSONObject();
+        payloadGameState.put(MessageType.K_TYPE, MessageType.T_GAME_STATE);
+
+        JSONObject ball = new JSONObject();
+        ball.put(MessageType.X, game.getBallX());
+        ball.put(MessageType.Y, game.getBallY());
+        payloadGameState.put(MessageType.F_BALL, ball);
+
+        JSONObject paddles = new JSONObject();
+        paddles.put(MessageType.P1,
+            new JSONObject().put(MessageType.Y, game.getPaddleP1Y()));
+        paddles.put(MessageType.P2,
+            new JSONObject().put(MessageType.Y, game.getPaddleP2Y()));
+        payloadGameState.put(MessageType.F_PADDLES, paddles);
+
+        JSONObject score = new JSONObject();
+        score.put(MessageType.P1, game.getScoreP1());
+        score.put(MessageType.P2, game.getScoreP2());
+        payloadGameState.put(MessageType.F_SCORE, score);
+
+        payloadGameState.put(MessageType.F_TIMESTAMP, Instant.now().toString());
+
+        return payloadGameState;
+    }
 }
