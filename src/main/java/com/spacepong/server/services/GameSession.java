ba523ed9fb29e913c81064f5206096b2164058d3
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-11-24 17:41:57 +0100

    Fixed bug where connection issues could give value null and stop the gameThread

--- src/main/java/com/spacepong/server/services/GameSession.java
+++ src/main/java/com/spacepong/server/services/GameSession.java
@@ -14,6 +14,7 @@ import com.spacepong.server.states.PlayingState;
 public class GameSession {
     private final Player p1, p2;
     private WebSocket socketRPi;
+    private boolean isRPiConnected = false;
     private final int gameId;
     private final Game game;
     private final PlayerRegistry playerRegistry;
@@ -29,7 +30,10 @@ public class GameSession {
         this.gameId = gameId;
         this.game = new Game();
         this.playerRegistry = playerRegistry;
-        if (playerRegistry.getSocketRPi() != null) { this.socketRPi = playerRegistry.getSocketRPi(); }
+        if (playerRegistry.getSocketRPi() != null) {
+            this.socketRPi = playerRegistry.getSocketRPi();
+            isRPiConnected = true;
+        }
 
         this.countdownState = new CountdownState();
         this.playingState = new PlayingState();
@@ -75,10 +79,11 @@ public class GameSession {
         if (socket2 != null && socket2.isOpen()) {
             socket2.send(message.toString());
         }
-        if (socketRPi != null & socketRPi.isOpen()) {
+        if (isRPiConnected && socketRPi != null && socketRPi.isOpen()) {
             socketRPi.send(message.toString());
         }
-        boolean lostAnyConnection = socket1 == null || socket2 == null || !socket1.isOpen() || !socket2.isOpen();
+        boolean lostAnyConnection = socket1 == null || socket2 == null || !socket1.isOpen() || !socket2.isOpen() || socketRPi == null;
+        if (isRPiConnected) { lostAnyConnection = lostAnyConnection || socketRPi == null; }
         if (lostAnyConnection) {
             this.changeState(StateType.FINISHED);
         }
